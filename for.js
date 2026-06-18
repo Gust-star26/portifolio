bbdde5dfba1f8903e644bdb0dc40945be5ec1b47
Gust-star26 <glimadecarvalho69@gmail.com>
2026-06-18 11:36:29 -0300

    aula

---
+++ for.js
@@ -0,0 +1,5 @@
+let notas = [7, 8, 6];
+
+for (let i = 0; i < notas.length; i++) {
+    console.log("Notas:", notas[i]);
+}
--- tarefa.js
+++ tarefa.js
@@ -1,4 +1,3 @@
 let temingresso = false
 let maiorIdade = true
 console.log('pode entrar na festa?', temingresso && maiorIdade)
-ss
